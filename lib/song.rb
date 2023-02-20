3dc0b43daa42dca979c02c7e21d2f9f171625609
yurubahmed <yurubiftin2002@gmail.com>
2023-02-20 09:29:28 -0500

    commit tree 3b6721289b2c6967484d36d6e151967b6162b038

--- lib/song.rb
+++ lib/song.rb
@@ -7,4 +7,25 @@ class Song
     @album = album
   end
 
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.album)
+  end
+
+
 end
